4272f2a749700d446c00308d5cf9f728292b2e6f
collinwalker <collinwalkerdesigns@gmail.com>
2016-03-06 18:44:33 -0800

    started down the road to create the landing page

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# weekli
+# weekli.js
+---
+**Version 0.1.5**
 
-Pure javascript component. Users can quickly select their availability during a week, workweek, or weekend.
+[weekli.js](collnwalkr.github.io/weekli/) is the easiest way for your users to input their availability on a week calendar effortlessly.
+
+#### Get your hands on it
+[Bower](http://bower.io) install
+```bash
+bower install weekli.js --save
+```
+[Github](https://github.com/) clone
+```bash
+git clone https://github.com/collnwalkr/weekli.git
+```
+
+I just want a ZIP [download](https://github.com/collnwalkr/weekli/releases/download/0.1.5/weekli_js.zip)
--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var weekli_id = new Weekli({
     week: 'workweek',
     week_days: ['Saturday', 'Tuesday', 'Friday'],
     minute_interval: 60,
-    time_range: '7,24:00'
+    time_range: '7,13:00'
 });
 
 var dummydata = {"available":[{"day":"MON","time":"7:00-8:00"},{"day":"THU","time":"7:00-8:00"},{"day":"MON","time":"8:00-9:00"},{"day":"THU","time":"8:00-9:00"},{"day":"TUE","time":"10:00-11:00"},{"day":"WED","time":"10:00-11:00"},{"day":"FRI","time":"10:00-11:00"},{"day":"WED","time":"11:00-12:00"}],"unavailable":[{"day":"TUE","time":"7:00-8:00"},{"day":"WED","time":"7:00-8:00"},{"day":"FRI","time":"7:00-8:00"},{"day":"TUE","time":"8:00-9:00"},{"day":"WED","time":"8:00-9:00"},{"day":"FRI","time":"8:00-9:00"},{"day":"MON","time":"9:00-10:00"},{"day":"TUE","time":"9:00-10:00"},{"day":"WED","time":"9:00-10:00"},{"day":"THU","time":"9:00-10:00"},{"day":"FRI","time":"9:00-10:00"},{"day":"MON","time":"10:00-11:00"},{"day":"THU","time":"10:00-11:00"},{"day":"MON","time":"11:00-12:00"},{"day":"TUE","time":"11:00-12:00"},{"day":"THU","time":"11:00-12:00"},{"day":"FRI","time":"11:00-12:00"},{"day":"MON","time":"12:00-1:00"},{"day":"TUE","time":"12:00-1:00"},{"day":"WED","time":"12:00-1:00"},{"day":"THU","time":"12:00-1:00"},{"day":"FRI","time":"12:00-1:00"}]};
@@ -21,8 +21,3 @@ weekli_id.build();
 var load_data = function(){
     weekli_id.load_data(dummydata);
 };
-
-var weekli = new Weekli({
-    time_range: '0,10'
-});
-weekli.build();
